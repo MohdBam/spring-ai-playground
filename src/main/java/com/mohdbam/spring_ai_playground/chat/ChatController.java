9cf392e7909a63b77ccd9c85ca87c29c000bd75d
mohdbam <mohdbam2010@gmail.com>
2025-07-10 22:03:28 +0800

    feat(chat): add streaming & ChatResponse

--- src/main/java/com/mohdbam/spring_ai_playground/chat/ChatController.java
+++ src/main/java/com/mohdbam/spring_ai_playground/chat/ChatController.java
@@ -4,29 +4,50 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
 
     private final ChatClient chatClient;
-    
+
     public ChatController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder
-        .defaultAdvisors(MessageChatMemoryAdvisor.builder(MessageWindowChatMemory.builder().build()).build())
-        .build();
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(MessageWindowChatMemory.builder().build()).build())
+                .build();
     }
 
     @GetMapping
     public String chat(@RequestParam String message, @RequestParam String id) {
         return chatClient.prompt()
-        .user(message)
-        .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, id))
-        .call()
-        .content();
-    }   
+                .user(message)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, id))
+                .call()
+                .content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream(@RequestParam String message, @RequestParam String id) {
+        return chatClient.prompt()
+                .user(message)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, id))
+                .stream()
+                .content();
+    }
+
+    @GetMapping("/joke")
+    public ChatResponse getJoke() {
+        return chatClient.prompt()
+                .user("Tell me a dad joke about spring boot")
+                .call()
+                .chatResponse();
+    }
+
 }
